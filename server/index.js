cadfcdc531f256c681306259282f6c30a2776f36
Alexander Nguyen <alexandernguyen@Alexanders-MacBook-Pro-2.local>
2021-04-04 09:44:48 -0700

    init express server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
